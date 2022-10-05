985cf94fc0ddd11e48c170fe86be3cdf0f022525
sqlote <tms1csgo@gmail.com>
2022-10-05 19:32:49 -0300

    updating changes

--- App.py
+++ App.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+# Mysql Connection =D
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'fproject'
 mysql = MySQL(app)
+
+# setting
+app.secre_key = 'mysecretkey'
 
 @app.route('/')
 def Index():
